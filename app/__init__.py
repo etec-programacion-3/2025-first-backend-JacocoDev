b0fedb0141ccdcdbbdda4c6cda92f95f8dc69403
JacocoDev <joaquinmanescau07@gmail.com>
2025-04-16 13:01:59 -0300

    b. Implementar la conexión a la base de datos

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # Configuración básica
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///biblioteca.db'
+    # Ruta absoluta a la base de datos SQLite
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    db_path = os.path.join(basedir, '..', 'instance', 'library.db')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    from .routes import libro_bp
-    app.register_blueprint(libro_bp)
+    from . import models  # 👈 Esta línea es la clave
 
     return app
